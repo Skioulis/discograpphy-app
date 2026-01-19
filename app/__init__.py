30d2e6586e73f27a6805218c10a338b71816fff9
skioulis <skioulis@gmail.com>
2026-01-19 21:00:16 +0200

    Integrate reusable navbar component, improve base template structure, configure environment-based settings, and update CSS for navbar styling

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,20 @@
+import os
 from flask import Flask
-
+from dotenv import load_dotenv
 
 def create_app():
     app = Flask(__name__)
+
+
+    env = os.getenv('FLASK_ENV', 'development')
+
+    if env == 'production':
+        load_dotenv('.env.production')
+    else:
+        load_dotenv('.env.development')
+
+    # Now you can access variables using os.getenv()
+    app.config['DATABASE_URL'] = os.getenv('DB_URL')
 
     # Import and register the blueprint from app.py
     from .app import main_bp
