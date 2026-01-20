f6b676ed9cae351c4860f18e96dbcd909d7adad6
skioulis <skioulis@gmail.com>
2026-01-20 14:33:48 +0200

    Refactor navbar styling, update route naming, enhance navbar template with logo and dynamic links, and introduce new image asset for branding

--- app/app.py
+++ app/app.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
 
